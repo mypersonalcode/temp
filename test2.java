214cd4055cbd1a2fbeb42cc9fa8e3624f278f042
codeWTMSB <mazhichao12345@163.com>
2018-09-15 16:07:33 +0800

    test2
    这个一个test题，如何用一个数组实现3个栈

---
+++ test2.java
@@ -0,0 +1,33 @@
+public class test2 {
+    int stackSize = 100;
+    int[] buffer = new int[3 * stackSize];
+    int[] stackPointer = {-1, -1, -1};
+
+    public void push(int stackNum, int value) throws Exception {
+        if (stackPointer[stackNum] + 1 >= stackSize)
+            throw new Exception("Out of space.");
+        stackPointer[stackNum]++;
+        buffer[absTopOfStack(stackNum)] = value;
+    }
+
+    public int pop(int stackNum) throws Exception {
+        if (stackPointer[stackNum] == -1)
+            throw new Exception("Trying to pop an empty stack.");
+        int value = buffer[absTopOfStack(stackNum)];
+        buffer[absTopOfStack(stackNum)] = 0;
+        stackPointer[stackNum]--;
+        return value;
+    }
+
+    public int peek(int stackNum) {
+        return buffer[absTopOfStack(stackNum)];
+    }
+
+    public boolean isEmpty(int stackNum) {
+        return stackPointer[stackNum] == -1;
+    }
+
+    public int absTopOfStack(int stackNum) {
+        return stackNum * stackSize + stackPointer[stackNum];
+    }
+}
